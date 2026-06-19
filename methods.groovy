1b0573d605e0b66fa03bffe8373cbc48c7868747
NURHANA <nurhanaaminulla10@gmail.com>
2026-06-19 13:23:08 +0800

    ud

--- methods.groovy
+++ methods.groovy
@@ -3,5 +3,5 @@ def greetPlayer(name, level = 1) {
     "Welcome $name! You are level $level." 
 }
 
-println greetPlayer("Nur")       // Output: Welcome Nur! You are level 1.
-println greetPlayer("Adi", 5)  // Output: Welcome Adi! You are level 5.
+println greetPlayer("Nur")       
+println greetPlayer("Adi", 5)  
